5a9bdd826ee37d9b04e0bcc2bf5e86605241a943
avinashnadkar <avinashnadkar3@gmail.com>
2022-07-19 00:31:19 +0530

    created user list with basic styling and structure  and provided unique key (uuid) for components

---
+++ src/Pages/Chat/ChatComponents/OnlineUsers.jsx
@@ -0,0 +1,106 @@
+import styles from "./ChatComponent.module.css";
+import {v4 as uuid} from 'uuid';
+
+const OnlineUsers = (props) => {
+
+    let dummyUsers = [
+        {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        },
+        {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        },  {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        },  {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        },  {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        },  {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        }, {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        }, {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        }, {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        }, {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        }, {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        }, {
+            name : 'kakashi',
+            isOnline : 'false',
+            lastMessage: 'Last Message',
+            newMsgCount : 2
+        }
+    ]
+
+    return(
+        <div className={styles.users}>
+            <div className={styles.userAccount}>
+                <img src={"https://via.placeholder.com/300"} alt="" className={styles.chatProfilePic}/>
+            </div>
+
+            <div>
+                <div className={styles.userSearchBar}>
+                    <button className={styles.searchBtn}><svg viewBox="0 0 24 24" width="24" height="24" className=""><path fill="currentColor" d="M15.009 13.805h-.636l-.22-.219a5.184 5.184 0 0 0 1.256-3.386 5.207 5.207 0 1 0-5.207 5.208 5.183 5.183 0 0 0 3.385-1.255l.221.22v.635l4.004 3.999 1.194-1.195-3.997-4.007zm-4.808 0a3.605 3.605 0 1 1 0-7.21 3.605 3.605 0 0 1 0 7.21z"></path></svg></button>
+                    <input type="text" placeholder="Search.." className={styles.inputBox}/>
+                </div>
+            </div>
+
+            <div className={styles.userList}>
+            {
+                dummyUsers.map((el)=>{
+                    return(
+                        <div className={styles.user} key={uuid()}>
+                            <img src={"https://via.placeholder.com/300"} alt="" className={styles.chatProfilePic}/>
+                            <div className={styles.userName}>
+                                <p>{el.name}</p>
+                                <p>{el.lastMessage}</p>
+                            </div>
+                            <div className={styles.msgCount}>
+
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            </div>
+        </div>
+    )
+}
+
+export default OnlineUsers;
