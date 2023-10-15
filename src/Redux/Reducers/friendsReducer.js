715a038d40499feb142a257392162121c935963c
avinashnadkar <avinashnadkar3@gmail.com>
2023-10-15 20:46:07 +0530

    Added features to accept, reject, cancel, send a friend request

--- src/Redux/Reducers/friendsReducer.js
+++ src/Redux/Reducers/friendsReducer.js
@@ -1,5 +1,7 @@
 const initState = {
-    myFriends : []
+    myFriends : [],
+    friendRequests : [],
+    friendRequestsSent : []
 }
 
 const friendsReducer = (state = initState,action) => {
@@ -8,6 +10,20 @@ const friendsReducer = (state = initState,action) => {
         return{
             ...state,
             myFriends : action.payload
+        }
+    }
+
+    if(action.type === "setFriendRequests"){
+        return{
+            ...state,
+            friendRequests : action.payload
+        }
+    }
+
+    if(action.type === "setFriendRequestsSent"){
+        return{
+            ...state,
+            friendRequestsSent : action.payload
         }
     }
 
